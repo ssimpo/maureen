30fe4bab95dfa17db3592047535699aa7e82c4d1
Stephen Simpson <me@simpo.org>
2013-10-16 20:02:43 +0100

    Reworked the include from admin includes so it attempts to calculate even if the wp-admin directory is moved.  However it might fail if website paths are not directly mapped above the root.

--- includes/install.php
+++ includes/install.php
@@ -3,7 +3,7 @@ if(!defined('MAUREEN_INCLUDES_PATH')) {
    die('Access to this file is prohibited');
 }
 
-maureen::loadWpInclude('upgrade.php');
+maureen::loadWpAdminInclude('upgrade');
 class maureen_install {
 	public static function setup() {
 		
