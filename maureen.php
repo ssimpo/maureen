5d74c841cb441619b20f42c2043cb401b9365d15
me <me@simpo.org>
2013-10-16 13:06:08 +0100

    Added base plugin files.

---
+++ maureen.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Maureen
+ * Plugin URI: http://simpo.org/maureen
+ * Description: Maureen is a Social media plugin/widget for Wordpress that displays a tweet stream. It is build using: Wordpress, PHP, Dojo and Javascript.
+ * Version: 0.0.1
+ * Author: Stephen Simpson
+ * Author URI: http://simpo.org/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+?>
